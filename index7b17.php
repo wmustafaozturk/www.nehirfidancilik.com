45902a31c8cdafab27457a91a4e8af6e69a97c6a
wmustafaozturk <30618538+wmustafaozturk@users.noreply.github.com>
2019-10-16 16:58:25 +0300

    GIT YUKLEME
    GIT YUKLEME

---
+++ index7b17.php
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! 1.5 - Open Source Content Management" -->
+<feed xmlns="http://www.w3.org/2005/Atom"  xml:lang="tr-tr">
+	<title type="text">Anasayfa</title>
+	<subtitle type="text">Nehir Fidancılık; Yapraklı, İbreli ve şekilli ürünlerimizle binlerce çesit bitkinin yetistiği ülkemizin dünyadaki elverişli önceliğini değerlendiriyoruz.</subtitle>
+	<link rel="alternate" type="text/html" href="http://nehirfidancilik.com"/>
+	<id>http://nehirfidancilik.com/tr/index.php</id>
+	<updated>2018-09-12T14:24:06Z</updated>
+	<generator uri="http://joomla.org" version="1.5">Joomla! 1.5 - Open Source Content Management</generator>
+<link rel="self" type="application/atom+xml" href="http://nehirfidancilik.com/tr/index.php?format=feed&amp;type=atom" />
+	<entry>
+		<title>Ön Sayfa</title>
+		<link rel="alternate" type="text/html" href="http://nehirfidancilik.com/tr/component/content/article/1-icerik/2-nehir-fidancilik.html"/>
+		<published>2012-12-20T01:22:52Z</published>
+		<updated>2012-12-20T01:22:52Z</updated>
+		<id>http://nehirfidancilik.com/tr/component/content/article/1-icerik/2-nehir-fidancilik.html</id>
+		<author>
+			<name>Administrator</name>
+		<email>murat@crea-direct.com</email>
+		</author>
+		<summary type="html">&lt;p style=&quot;text-align: center; line-height: 20px;&quot;&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;Binlerce çesit bitkinin yetistiği, dört mevsimin avantajını yaşayan ülkemizin dünyadaki elverişli önceliğini değerlendiriyoruz. A&lt;/span&gt;&lt;span&gt;&lt;/span&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;nadolu’nun en verimli bölgelerinden biri olarak kabul edilen &lt;strong&gt;Pamukova&lt;/strong&gt;’da, tüm fidanlarımızı sevgiyle ve dünya&lt;/span&gt;&lt;span&gt;&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;standartlarında yetiştiriyoruz.&lt;br /&gt;&lt;br /&gt;Toprağımızın verimi ve zenginliği sayesinde, tüm beğenilere karşılık verebilecek bir hizmet&lt;/span&gt;&lt;span&gt;&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;vadediyoruz.&lt;br /&gt;&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif; color: #339966;&quot;&gt;Biz bu toprakların gücüne inanıyoruz...&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;text-align: center; line-height: 20px;&quot;&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif; color: #339966;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;/p&gt;
+&lt;p&gt;&lt;img src=&quot;http://nehirfidancilik.com/tr/images/front/1.png&quot; width=&quot;686&quot; height=&quot;216&quot; alt=&quot;1&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; /&gt;&lt;/p&gt;</summary>
+		<content type="html">&lt;p style=&quot;text-align: center; line-height: 20px;&quot;&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;Binlerce çesit bitkinin yetistiği, dört mevsimin avantajını yaşayan ülkemizin dünyadaki elverişli önceliğini değerlendiriyoruz. A&lt;/span&gt;&lt;span&gt;&lt;/span&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;nadolu’nun en verimli bölgelerinden biri olarak kabul edilen &lt;strong&gt;Pamukova&lt;/strong&gt;’da, tüm fidanlarımızı sevgiyle ve dünya&lt;/span&gt;&lt;span&gt;&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;standartlarında yetiştiriyoruz.&lt;br /&gt;&lt;br /&gt;Toprağımızın verimi ve zenginliği sayesinde, tüm beğenilere karşılık verebilecek bir hizmet&lt;/span&gt;&lt;span&gt;&amp;nbsp;&lt;/span&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;&quot;&gt;vadediyoruz.&lt;br /&gt;&lt;/span&gt;&lt;br /&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif; color: #339966;&quot;&gt;Biz bu toprakların gücüne inanıyoruz...&lt;/span&gt;&lt;/p&gt;
+&lt;p style=&quot;text-align: center; line-height: 20px;&quot;&gt;&lt;span style=&quot;font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif; color: #339966;&quot;&gt;&amp;nbsp;&lt;/span&gt;&lt;/p&gt;
+&lt;p&gt;&lt;img src=&quot;http://nehirfidancilik.com/tr/images/front/1.png&quot; width=&quot;686&quot; height=&quot;216&quot; alt=&quot;1&quot; style=&quot;display: block; margin-left: auto; margin-right: auto;&quot; /&gt;&lt;/p&gt;</content>
+	</entry>
+</feed>
---
+++ indexc0d0.php
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!-- generator="Joomla! 1.5 - Open Source Content Management" -->
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+	<channel>
+		<title>Anasayfa</title>
+		<description>Nehir Fidancılık; Yapraklı, İbreli ve şekilli ürünlerimizle binlerce çesit bitkinin yetistiği ülkemizin dünyadaki elverişli önceliğini değerlendiriyoruz.</description>
+		<link>http://nehirfidancilik.com/tr/component/content/frontpage.html</link>
+		<lastBuildDate>Wed, 12 Sep 2018 14:24:06 +0000</lastBuildDate>
+		<generator>Joomla! 1.5 - Open Source Content Management</generator>
+		<language>tr-tr</language>
+		<item>
+			<title>Ön Sayfa</title>
+			<link>http://nehirfidancilik.com/tr/component/content/article/1-icerik/2-nehir-fidancilik.html</link>
+			<guid>http://nehirfidancilik.com/tr/component/content/article/1-icerik/2-nehir-fidancilik.html</guid>
+			<description><![CDATA[<p style="text-align: center; line-height: 20px;"><span style="font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;">Binlerce çesit bitkinin yetistiği, dört mevsimin avantajını yaşayan ülkemizin dünyadaki elverişli önceliğini değerlendiriyoruz. A</span><span></span><span style="font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;">nadolu’nun en verimli bölgelerinden biri olarak kabul edilen <strong>Pamukova</strong>’da, tüm fidanlarımızı sevgiyle ve dünya</span><span>&nbsp;</span><span style="font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;">standartlarında yetiştiriyoruz.<br /><br />Toprağımızın verimi ve zenginliği sayesinde, tüm beğenilere karşılık verebilecek bir hizmet</span><span>&nbsp;</span><span style="font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif;">vadediyoruz.<br /></span><br /><span style="font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif; color: #339966;">Biz bu toprakların gücüne inanıyoruz...</span></p>
+<p style="text-align: center; line-height: 20px;"><span style="font-size: 12pt; font-family: tahoma, arial, helvetica, sans-serif; color: #339966;">&nbsp;</span></p>
+<p><img src="http://nehirfidancilik.com/tr/images/front/1.png" width="686" height="216" alt="1" style="display: block; margin-left: auto; margin-right: auto;" /></p>]]></description>
+			<author>murat@crea-direct.com (Administrator)</author>
+			<category>frontpage</category>
+			<pubDate>Thu, 20 Dec 2012 01:22:52 +0000</pubDate>
+		</item>
+	</channel>
+</rss>
